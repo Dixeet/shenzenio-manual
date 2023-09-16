7b693e5b8ed26108aa00b9b52558f78200fdc5c2
Robin <robin.lebert@gmail.com>
2023-09-16 12:26:23 +0200

    chore: basic config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,20 +2,20 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  srcDir: './src',
   title: "Shenzen IO Manual",
   description: "Shenzen IO Digital Manual",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/api-examples' }
     ],
 
     sidebar: [
       {
         text: 'Examples',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
         ]
       }
---
+++ src/index.md
@@ -0,0 +1,17 @@
+---
+titleTemplate: false
+---
+
+# Shenzen IO Manual
+
+## CheatSheet
+
+**Basic Instructions**
+
+```
+nop
+mov R/I R
+jmp L
+slp R/I
+slx P
+```
