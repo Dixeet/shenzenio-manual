35d1e7502a5e892082529e11ce64b9ceabfbc041
Robin <robin.lebert@gmail.com>
2023-09-17 13:28:28 +0200

    feat: add subtitle to pages

--- src/components/WideSubtitleBlock.vue
+++ src/components/WideSubtitleBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wide-subtitle-block --my-7">
+  <div class="wide-subtitle-block --my-7 --font-size-4">
     <slot/>
   </div>
 </template>
@@ -8,9 +8,19 @@
 
 </script>
 
-<style>
+<style lang="scss">
 .wide-subtitle-block {
   background-color: var(--vp-code-block-bg);
   text-align: center;
+  text-transform: uppercase;
+  h2 {
+    font-size: 1rem;
+    margin: 0;
+    padding: 0;
+    border: none;
+    line-height: inherit;
+    font-weight: inherit;
+    letter-spacing: inherit;
+  }
 }
 </style>
