c372ce050084e6e9100adf8622f630b96f988695
Robin <robin.lebert@gmail.com>
2023-09-17 13:53:11 +0200

    feat: add 100p-14 parts

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -49,6 +49,7 @@ export default defineConfig({
               { text: 'MC4000', link: '/parts/mc4000' },
               { text: 'MC6000', link: '/parts/mc6000' },
               { text: 'DX300', link: '/parts/dx300' },
+              { text: '100P-14', link: '/parts/100p-14' },
             ]
           },
         ]
